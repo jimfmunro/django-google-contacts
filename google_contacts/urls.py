21ac15cf6096e338823678a06d50107c36ba4822
Jim Munro <jimfmunro@gmail.com>
2014-06-06 06:26:45 -0600

    update urls to work with >= django 1.5

--- google_contacts/urls.py
+++ google_contacts/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
 
 from views import google_get_state_token, google_login, google_logout
 
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     url(r'^get-state-token/(?P<action_type_id>\d+)/(?P<action_id>\d+)/$', google_get_state_token, name='google_contacts_get_state_token'),
     url(r'^login/$', google_login, name='google_contacts_login'),
     url(r'^logout/$', google_logout, name='google_contacts_logout'),
-    
+
 )
